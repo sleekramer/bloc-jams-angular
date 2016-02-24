ee3c69b1e626e15706bc8f00703294c751ba69bf
Steven Lee-Kramer <sleekramer@gmail.com>
2016-02-24 16:39:19 -0500

    complete assignment: add playSong() private funciton in SongPlayer service, which plays the currentBuzzObject and sets the .playing property of the current song to true.

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,6 +1,10 @@
 (function () {
     function SongPlayer() {
         var SongPlayer = {};
+        /**
+        * @desc song object from Fixtures.js
+        * @type {Object}
+        */
         var currentSong = null;
         /**
         * @desc Buzz object audio file
@@ -23,22 +27,37 @@
                 });
                 currentSong = song;
         };
-    
+        /**
+        * @function playSong
+        * @desc plays the curentBuzzObject and sets .playing property of song to true
+        * @param {Object} song
+        **/
+        var playSong = function (song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        /**
+        * @function SongPlay.play
+        * @desc sets and plays song 
+        * @param {Object} song
+        */
         SongPlayer.play = function (song) {
             
             if (currentSong !== song) {
                 setSong(song);
-            
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
+                
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
-                    song.playing = true;
+                   playSong(song);
                 }
             }
         };
-        
+        /** 
+        * @function SongPlayer.pause
+        * @desc pauses the currently playing song (param)
+        * @param {Object} song
+        */
         SongPlayer.pause = function (song) {
             currentBuzzObject.pause();
             song.playing = false;
