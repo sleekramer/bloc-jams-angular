92203ca724e244e47f6cf608dcb6a75512720dc2
Steven Lee-Kramer <sleekramer@gmail.com>
2016-02-25 14:25:54 -0500

    complete checkpoint: add functionality to seekBar directive, pass value, max, and onChange attributes to the directive and observe changes to them with attributes.(), use these attributes and additions to SongPlayer service to upate the seek-bar position and currentTime of the currently playing song.

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function () {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         var SongPlayer = {};
         /**
         * @desc stores the current album object from Fixtures service
@@ -24,6 +24,13 @@
                     formats: ['mp3'],
                     preload: true
                 });
+            
+                currentBuzzObject.bind('timeupdate', function () {
+                    $rootScope.$apply(function() {
+                        SongPlayer.currentTime = currentBuzzObject.getTime(); 
+                    });
+                });    
+            
                 SongPlayer.currentSong = song;
         };
         /**
@@ -59,6 +66,11 @@
         * @type {Object}
         */
         SongPlayer.currentSong = null;
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
         /**
         * @function SongPlayer.play
         * @desc sets and plays song 
@@ -121,12 +133,22 @@
                 setSong(song);
                 playSong(song);
             }
-        }
+        };
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        SongPlayer.setCurrentTime = function (time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }  
+        };
         
         return SongPlayer;
     }
     
     angular 
         .module('blocJams')
-        .factory('SongPlayer', ["Fixtures", SongPlayer]);
+        .factory('SongPlayer', ["$rootScope", "Fixtures", SongPlayer]);
 })();
